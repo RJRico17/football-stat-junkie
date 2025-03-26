86b6db31b958ccd19d7d829470bce863c43c673e
RJ <fredrickalexander1727@gmail.com>
2025-03-26 02:44:45 -0700

    learning some cool stuff

--- scraper.py
+++ scraper.py
@@ -1,15 +1,22 @@
 from bs4 import BeautifulSoup
 import requests
 
-page_to_scrape = requests.get('https://quotes.toscrape.com/')
-soup = BeautifulSoup(page_to_scrape.text, "html")
+page_to_scrape = requests.get('https://matchroompool.com/players/?_gl=1*qek46d*_up*MQ..*_ga*MTI4Nzk4MDczMi4xNzQyOTY3NDQ1*_ga_N5ZWBCJD2K*MTc0Mjk2NzQ0My4xLjAuMTc0Mjk2NzQ0My4wLjAuMA..')
+soup = BeautifulSoup(page_to_scrape.text, "html.parser")
 
-print(soup)
-quotes = soup.find_all("span", attrs={"class":"text"})
 
-authors = soup.find_all("small", attrs={"class":"author"})
+main_div = soup.find("div", attrs={"data-type":"players"})
+firsts = main_div.find_all("span", {"class":"first"},limit=5)
+lasts = main_div.find_all("span", {"class":"last"},limit=5)
+countrys = main_div.find_all("div", {"class":"country"},limit=5)
+ranks = main_div.find_all("div", {"class":"rank"},limit=5)
 
-for quote in quotes:
-    print(quote.text)
-for author in authors:
-    print(author.text)
+for first, last in zip(firsts,lasts):
+    player_scrape = requests.get('https://en.wikipedia.org/wiki/'+first.text+"_"+last.text)
+    soup = BeautifulSoup(player_scrape.text, "html.parser")
+    bdays = soup.find("span",attrs={"class":"bday"})
+
+for rank, first, last, country, bday in zip(ranks, firsts,lasts,countrys,bdays):
+    print(rank.text + ": " + first.text + " " + last.text + " - " + country.text + " - " + bday.text)
+    print('https://www.azbilliards.com/person/'+first.text+"-"+last.text)
+    
