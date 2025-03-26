1e4bee0e04397ed47202606a470025ed76e452e0
RJ <fredrickalexander1727@gmail.com>
2025-03-25 22:31:49 -0700

    inital commit

---
+++ scraper.py
@@ -0,0 +1,15 @@
+from bs4 import BeautifulSoup
+import requests
+
+page_to_scrape = requests.get('https://quotes.toscrape.com/')
+soup = BeautifulSoup(page_to_scrape.text, "html")
+
+print(soup)
+quotes = soup.find_all("span", attrs={"class":"text"})
+
+authors = soup.find_all("small", attrs={"class":"author"})
+
+for quote in quotes:
+    print(quote.text)
+for author in authors:
+    print(author.text)
