299e4f3f3d794b28dede9fd2c8656e8987823c53
RJ <fredrickalexander1727@gmail.com>
2025-03-26 20:39:37 -0700

    messing around w pandas

--- scraper.py
+++ scraper.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
+import sys
 
 page_to_scrape = requests.get('https://matchroompool.com/players/?_gl=1*qek46d*_up*MQ..*_ga*MTI4Nzk4MDczMi4xNzQyOTY3NDQ1*_ga_N5ZWBCJD2K*MTc0Mjk2NzQ0My4xLjAuMTc0Mjk2NzQ0My4wLjAuMA..')
 soup = BeautifulSoup(page_to_scrape.text, "html.parser")
@@ -11,11 +13,10 @@ lasts = main_div.find_all("span", {"class":"last"})
 countrys = main_div.find_all("div", {"class":"country"})
 ranks = main_div.find_all("div", {"class":"rank"})
 
-# for first, last in zip(firsts,lasts):
-#     player_scrape = requests.get('https://en.wikipedia.org/wiki/'+first.text+"_"+last.text)
-#     soup = BeautifulSoup(player_scrape.text, "html.parser")
-#     bdays = soup.find("span",attrs={"class":"bday"})
-
 for rank, first, last, country in zip(ranks, firsts,lasts,countrys):
     print(rank.text + ": " + first.text + " " + last.text + " - " + country.text)
     print('https://www.azbilliards.com/person/'+first.text+"-"+last.text)
+
+
+df = pd.read_csv('seahawks_roster.csv')
+print(df)
